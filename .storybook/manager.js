9adc0268c54637c210c06156ef7c2e8236deed57
Kehlween <contato.zacha@gmail.com>
2021-09-01 17:50:36 -0300

    Header adicionado

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import ediaristasTheme from './zascha-theme';
+
+addons.setConfig({
+    theme: ediaristasTheme,
+});
---
+++ .storybook/zascha-theme.js
@@ -0,0 +1,7 @@
+import {create} from '@storybook/theming';
+
+export default create({
+    brandTitle: 'Zascha',
+    brandImage: './img/logos/zascha_logo05.png',
+    brandUrl: 'https://zascha.com.br',
+});
